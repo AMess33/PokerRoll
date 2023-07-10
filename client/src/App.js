c3d4a4a06bcad34cdad3f0857a8264ad747ed677
AMess33 <amess3309@gmail.com>
2023-07-09 20:52:07 -0500

    start building app.js and add bottomnav to app function return

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,49 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BottomNav from "./components/BottomNav";
+import Footer from "./components/Footer";
 import "./App.css";
 
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="App">
+          <BottomNav />
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
 }
 
 export default App;
