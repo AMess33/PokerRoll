79dd76b4a53f9e303f0ddc46530c00d11c280363
AMess33 <amess3309@gmail.com>
2023-07-11 12:33:34 -0500

    start building session schemas

--- server/models/Session.js
+++ server/models/Session.js
@@ -1,7 +1,50 @@
+const { Schema, model } = require("mongoose");
+
+const sessionSchema = new Schema({
+  // gameType: {[cashgame | tournament]},
+  casino: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+});
+
 // sessions will have
 // casino
 // location
-
+const cashGameSchema = new Schema({
+  buyIn: {
+    type: Int,
+    required: true,
+  },
+  cashOut: {
+    type: Int,
+    required: true,
+  },
+  stakes: {
+    type: String,
+    required: true,
+  },
+  gameType: {
+    type: String,
+    required: true,
+  },
+  start: {
+    // type: time,
+    // required: true,
+  },
+  endTime: {
+    // type: time,
+    // required: true,
+  },
+  winLoss: {
+    type: Int,
+    required: true,
+  },
+});
 // cash game or tournament
 
 // cashgame
