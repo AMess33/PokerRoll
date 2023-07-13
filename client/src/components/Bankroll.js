0f446a934480021f9e80facf417f489a6afec047
AMess33 <amess3309@gmail.com>
2023-07-13 16:33:16 -0500

    bankroll boilerplate

--- client/src/components/Bankroll.js
+++ client/src/components/Bankroll.js
@@ -2,3 +2,14 @@
 // add to bankroll
 // subtract from bankroll
 // view most recent sessions w/ +- of each
+import { Link } from "react-router-dom";
+
+function Bankroll() {
+  return (
+    <div>
+      <h3>$ ${bankroll}</h3>
+    </div>
+  );
+}
+
+export default Bankroll;
