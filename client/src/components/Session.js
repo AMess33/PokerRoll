ebd76ef7b8c7a922268bb3689e67abc428849262
AMess33 <amess3309@gmail.com>
2023-07-13 16:27:33 -0500

    boilerplate session compon

--- client/src/components/Session.js
+++ client/src/components/Session.js
@@ -1,3 +1,9 @@
 // start cash game button
 // start tournament button
 // view previous previous sessions/most recent first
+
+import { Link } from "react-router-dom";
+
+function Session() {
+  return <div>{/* <H3>session name</H3> */}</div>;
+}
