f8d816c9cced97ab923865cdf2ab1fddb52be47c
AMess33 <amess3309@gmail.com>
2023-07-13 16:19:50 -0500

    boilerplate header compon

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,1 +1,14 @@
 // simple title logo on header/ bottom nav will serve as navbar
+import React from "react";
+import { Link } from "react-router-dom";
+import Auth from "../utils/auth";
+
+function Header() {
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+  return <header></header>;
+}
+
+export default Header;
