a64cad9037a327e3019509ae95cc6c8b67e2323d
AMess33 <amess3309@gmail.com>
2023-07-16 23:11:43 -0500

    start to build tournament form

--- client/src/components/TournamentForm.js
+++ client/src/components/TournamentForm.js
@@ -4,11 +4,30 @@
 // casino
 // location
 // finished place/ and prize
-
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 import { Link } from "react-router-dom";
 
 function TournamentForm() {
-  return <div>{/* <H3>tournament name</H3> */}</div>;
+  return (
+    <div>
+      <div>
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField id="outlined-basic" label="Casino" variant="outlined" />
+          <TextField id="outlined-basic" label="Location" variant="outlined" />
+          <TextField id="outlined-basic" label="Game" variant="outlined" />
+          <TextField id="outlined-basic" label="Buy In" variant="outlined" />
+        </Box>
+      </div>
+    </div>
+  );
 }
 
 export default TournamentForm;
