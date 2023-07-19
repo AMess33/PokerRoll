7f2c24e836058eb44d046cfcca865c7386bfecd3
AMess33 <amess3309@gmail.com>
2023-07-18 23:33:51 -0500

    add bootstarp styles

--- client/src/components/BottomNav.js
+++ client/src/components/BottomNav.js
@@ -12,6 +12,7 @@ export default function BottomNav() {
   return (
     <Box sx={{ width: 500 }}>
       <BottomNavigation
+        className="text-danger"
         showLabels
         value={value}
         onChange={(event, newValue) => {
