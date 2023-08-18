1afbd7b04a94e726502e31eb4de60614da45e41a
AMess33 <amess3309@gmail.com>
2023-08-17 22:46:33 -0500

    add router and paths to main for root, tournament, bankroll, and cashgame

--- apps/poker-roll/src/main.tsx
+++ apps/poker-roll/src/main.tsx
@@ -1,13 +1,37 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 import App from './app/app';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "/bankroll",
+        element: <Bankroll />,
+        loader: bankrollLoader,
+      },
+      {
+        path: "/cashgame",
+        element: <CashGame />,
+        loader: cashgameLoader,
+      },
+      {
+        path: "/tournament",
+        element: <Tournament />,
+        loader: tournamentLoader,
+      }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
