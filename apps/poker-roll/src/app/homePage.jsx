a21f78915aaf61e487586b82c40a27181cb14b75
AMess33 <amess3309@gmail.com>
2023-08-18 23:05:05 -0500

    start building homepage layout. add bottom nav to app with buttons for bankroll cashgame and tournament

--- apps/poker-roll/src/app/homePage.jsx
+++ apps/poker-roll/src/app/homePage.jsx
@@ -1,13 +1,40 @@
 import React from "react";
 import Outlet from "react-router-dom";
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import TableBarIcon from '@mui/icons-material/TableBar';
+import PaidIcon from '@mui/icons-material/Paid';
 
 const Home = (props) => {
 
     return (
         <div>
-            <div></div>
-            <Outlet />
+            <div className="header">
+                <h1>Poker Roll</h1>
+            </div>
+            <div>
+                <Outlet />
+            </div>
+            <div className="footer">
+            <Box sx={{ width: 500 }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="BankRoll" icon={<PaidIcon />} />
+        <BottomNavigationAction label="CashGames" icon={<TableBarIcon />} />
+        <BottomNavigationAction label="Tournament" icon={<EmojiEventsIcon />} />
+      </BottomNavigation>
+    </Box>
+            </div>
         </div>
     )
-}
+};
+
+
 export default Home;
