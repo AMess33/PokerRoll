571c829884882859911818e70994a0732e5f135e
AMess33 <amess3309@gmail.com>
2023-08-18 20:02:57 -0500

    import outlet into homepage

--- apps/poker-roll/src/app/homePage.jsx
+++ apps/poker-roll/src/app/homePage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+import Outlet from "react-router-dom";
 
 const Home = (props) => {
 
     return (
         <div>
             <div></div>
+            <Outlet />
         </div>
     )
 }
