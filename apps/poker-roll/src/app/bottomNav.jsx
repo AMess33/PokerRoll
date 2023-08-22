aa760b4b801ead6d78d9824e1c0228ff3b9567ae
AMess33 <amess3309@gmail.com>
2023-08-21 20:04:36 -0500

    wrap bottom nav actions in a link so the button would navigate to a new webpage for /cashgame /tournament and /bankroll

--- apps/poker-roll/src/app/bottomNav.jsx
+++ apps/poker-roll/src/app/bottomNav.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Link from '@mui/material/Link';
 import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
@@ -11,20 +11,20 @@ export default function BottomNav() {
     const [value, setValue] = React.useState(0);
   
     return (
-<div className="footer">
-                <Box sx={{ width: 500 }}>
-                    <BottomNavigation
-                     showLabels
-                     value={value}
-                        onChange={(event, newValue) => {
+        <div className="footer">
+            <Box sx={{ width: 500 }}>
+                <BottomNavigation
+                 showLabels
+                 value={value}
+                    onChange={(event, newValue) => {
                     setValue(newValue);
                 }}
-      >
-        <BottomNavigationAction label="BankRoll" value="bankroll" icon={<PaidIcon />} />
-        <BottomNavigationAction label="CashGames" value="cashgame" icon={<TableBarIcon />} />
-        <BottomNavigationAction label="Tournament" value="tournament" icon={<EmojiEventsIcon />} />
-      </BottomNavigation>
-                    </Box>
-            </div>)
+            >
+                    <Link href="/bankroll"><BottomNavigationAction label="BankRoll"  icon={<PaidIcon />} /></Link>
+                    <Link href="/cashgame"><BottomNavigationAction label="CashGames"  icon={<TableBarIcon />} /></Link>
+                    <Link href="/tournament"><BottomNavigationAction label="Tournament"  icon={<EmojiEventsIcon />} /></Link>
+                </BottomNavigation>
+             </Box>
+        </div>
+    );
 }
-
