6c0ecb804d3a50466df8253f9b237deef7c4ff9e
AMess33 <amess3309@gmail.com>
2023-08-23 17:52:46 -0500

    add form and stacks for the inputs when creating new session, cash or tournament

--- apps/poker-roll/src/app/startSession.jsx
+++ apps/poker-roll/src/app/startSession.jsx
@@ -27,6 +27,10 @@ const SessionForm = (props) => {
 
     return (
         <div>
+            <form
+        className="bg-white m-4 border border-dark rounded text-dark newAccountForm"
+        onSubmit={handleFormSubmit}
+      >
             <ToggleButtonGroup
       color="primary"
       value={alignment}
@@ -37,7 +41,114 @@ const SessionForm = (props) => {
       <ToggleButton value="cashgame">CashGame</ToggleButton>
       <ToggleButton value="tournament">Tournament</ToggleButton>
       </ToggleButtonGroup>
+      {/* change which stack is show depending on cashgame or tournament is selected */}
+        <Stack
+          spacing={2}
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <h2>Enter New Tournament</h2>
+          <TextField
+            label="Tournamnet Buy In"
+            variant="outlined"
+            name="buyIn"
+            type="text"
+            value={formState.name}
+            onChange={handleChange}
+            size="small"
+            margin="normal"
+          />
+          <TextField
+            name="name"
+            type="text"
+            value={formState.email}
+            label="Tournament Name"
+            onChange={handleChange}
+            variant="outlined"
+            size="small"
+            margin="normal"
+          />
+          <TextField
+            label="Casino"
+            variant="outlined"
+            name="casino"
+            type="text"
+            value={formState.password}
+            onChange={handleChange}
+            size="small"
+            margin="normal"
+          />
+            <TextField 
+            label="Location"
+            variant="outlined"
+            name="location"
+            type="text"
+            value={formState.password}
+            onChange={handleChange}
+            size="small"
+            margin="normal"
+          />
+          <Button
+            type="submit"
+            size="small"
+            variant="contained"
+            color="primary"
+          >
+            Begin Tournament
+          </Button>
+          <h2>New CashGame</h2>
+          <TextField
+            label="Initial Buy In"
+            variant="outlined"
+            name="buyIn"
+            type="text"
+            value={formState.name}
+            onChange={handleChange}
+            size="small"
+            margin="normal"
+          />
+          <TextField
+            name="name"
+            type="text"
+            value={formState.email}
+            label="Game and Stakes"
+            onChange={handleChange}
+            variant="outlined"
+            size="small"
+            margin="normal"
+          />
+          <TextField
+            label="Casino"
+            variant="outlined"
+            name="casino"
+            type="text"
+            value={formState.password}
+            onChange={handleChange}
+            size="small"
+            margin="normal"
+          />
+            <TextField 
+            label="Location"
+            variant="outlined"
+            name="location"
+            type="text"
+            value={formState.password}
+            onChange={handleChange}
+            size="small"
+            margin="normal"
+          />
+          <Button
+            type="submit"
+            size="small"
+            variant="contained"
+            color="primary"
+          >
+            Start CashGame
+          </Button>
 
+        </Stack>
+      </form>
         </div>
     )
 }
