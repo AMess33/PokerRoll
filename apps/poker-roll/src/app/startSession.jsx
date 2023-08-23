6b1eea35d2d5cc1a2a3146b0959700f6bea6a629
AMess33 <amess3309@gmail.com>
2023-08-23 10:58:44 -0500

    psuedo code for session form

--- apps/poker-roll/src/app/startSession.jsx
+++ apps/poker-roll/src/app/startSession.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { TextField, Button, Stack } from "@mui/material";
 
 
 const SessionForm = (props) => {
+    const [formState, setFormState] = useState({
+        // tournament or cash
+            // if tournament 
+            // buy in
+            // tournament name
+            // casino
+            // location
+                // if cash
+                // game type
+                // buy in
+                // casino
+                // location
+                // start time
+    })
 
     return (
         <div>
