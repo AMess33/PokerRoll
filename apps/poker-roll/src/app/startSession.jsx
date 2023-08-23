6a7bc1f24c8004c99956c069cf1f0cad3606a73f
AMess33 <amess3309@gmail.com>
2023-08-23 10:27:12 -0500

    create a new jsx file for the create session form

---
+++ apps/poker-roll/src/app/startSession.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const SessionForm = (props) => {
+
+    return (
+        <div>
+            <div>Enter new tournament Here</div>
+        </div>
+    )
+}
+export default SessionForm;
