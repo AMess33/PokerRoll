88c586c84e949c51557d72320b71bdd478404410
AMess33 <amess3309@gmail.com>
2023-08-27 21:34:51 -0500

    build out basic bankroll card component

--- apps/poker-roll/src/app/bankrollPage.jsx
+++ apps/poker-roll/src/app/bankrollPage.jsx
@@ -1,13 +1,30 @@
 import React from "react";
-
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
 
 const Bankroll = (props) => {
 
     return (
         <div>
-            <div>
-                Here is your current bankroll 
-            </div>
+            <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Current BankRoll Balance: $0.00
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Click to expand to view bankroll over time
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        Update Bankroll
+                    </Button>
+                </CardActions>
+            </Card>
         </div>
     )
 }
