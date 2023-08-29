243448f5d1a55a920e35b5abcc0f2d79bf107615
AMess33 <amess3309@gmail.com>
2023-08-28 23:53:10 -0500

    add container to bankroll page

--- apps/poker-roll/src/app/bankrollPage.jsx
+++ apps/poker-roll/src/app/bankrollPage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 const Bankroll = (props) => {
 
     return (
         <div>
-            <div>BankRoll Page</div>
+            <Container maxWidth="sm">
+                <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+            </Container>
         </div>
     )
 }
