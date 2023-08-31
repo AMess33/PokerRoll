250c95802860e1fbbf1d6e206b65051e6c614002
AMess33 <amess3309@gmail.com>
2023-08-31 18:18:32 -0500

    create graph componenet file, add boilerplate code

---
+++ apps/poker-roll/src/app/graphComponent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Graph = (props) => {
+
+    return(
+        <div>
+            <div>
+                {/* build graph here */}
+            </div>
+        </div>
+    )
+}
+export default Graph;
