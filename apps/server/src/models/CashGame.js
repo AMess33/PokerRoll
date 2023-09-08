7836d01de90a8e7d5daf27038d8b8124f3ecee98
AMess33 <amess3309@gmail.com>
2023-09-08 12:09:39 -0500

    build session schema in the cashgame file

--- apps/server/src/models/CashGame.js
+++ apps/server/src/models/CashGame.js
@@ -1,1 +1,18 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const sessionSchema = new Schema({
+  game: String,
+  title: String,
+  casino: String,
+  location: String,
+  inFor: Number,
+  outFor: Number,
+  // duration of session, difference of end and start times
+  date: { type: Date, default: Date.now },
+  plusMinus: Number,
+});
+
+const Session = mongoose.model('Session', sessionSchema);
+
+const handleError = (err) => console.error(err);
