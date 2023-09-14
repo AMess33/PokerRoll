5e6cafabf32e7f89d18c199f0f1c9a88746afca1
AMess33 <amess3309@gmail.com>
2023-09-13 22:37:43 -0500

    add User to injex.js

--- apps/server/src/models/index.js
+++ apps/server/src/models/index.js
@@ -1,4 +1,5 @@
-const Bankroll = require('./bankroll');
+const Bankroll = require('./Bankroll');
 const Session = require('./Session');
+const User = require('./User');
 
-module.exports = { Bankroll, Session };
+module.exports = { Bankroll, Session, User };
