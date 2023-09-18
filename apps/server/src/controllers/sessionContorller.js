4393df65a65a75d5844d9dd3a8f2c09d915e2fe7
AMess33 <amess3309@gmail.com>
2023-09-18 14:15:15 -0500

    build out create session and update session controller

--- apps/server/src/controllers/sessionContorller.js
+++ apps/server/src/controllers/sessionContorller.js
@@ -9,7 +9,31 @@ module.exports = {
   async createSession(req, res) {
     try {
       const session = await Session.create(req.body);
-      res.json(session);
+      const user = await User.findOneAndUpdate(
+        { _id: req.body.userId },
+        { $addToSet: { sessions: session._id } },
+        { new: true }
+      );
+      if (!user) {
+        return res.status(404).json({
+          message: 'Session created, but found no user with that ID',
+        });
+      }
+
+      res.json('New Session started');
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async updateSession(req, res) {
+    try {
+      const session = await Session.findOneAndUpdate(
+        { _id: req.params.sessionId },
+        { $set: req.body }
+      );
+      if (!session) {
+        return res.status(404).json({ message: 'No session with this id!' });
+      }
     } catch (err) {
       res.status(500).json(err);
     }
