592bea91011218e4bab885671681ad494141b104
AMess33 <amess3309@gmail.com>
2023-09-19 19:16:57 -0500

    add update bankroll to user routes

--- apps/server/src/routes/api/userRoutes.js
+++ apps/server/src/routes/api/userRoutes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 
 const { createUser } = require('../../controllers/userController');
+const { updateBankroll } = require('../../controllers/bankrollController');
 
 router.route('/').post(createUser);
+
+router.route('/').put(updateBankroll);
 
 module.exports = router;
