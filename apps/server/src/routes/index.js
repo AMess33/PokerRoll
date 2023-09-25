60261add3905a6cc64a109dcd74228bffd5f4f6d
AMess33 <amess3309@gmail.com>
2023-09-25 14:36:06 -0500

    put all routes in index.js in routes directory, update pathing of each to avoid conflicts

--- apps/server/src/routes/index.js
+++ apps/server/src/routes/index.js
@@ -6,3 +6,19 @@
 // module.exports = router;
 
 // move this code to server/src/main.js
+import { router } from '../../main';
+
+const { createUser, deleteUser } = require('../../controllers/userController');
+const { updateBankroll } = require('../../controllers/bankrollController');
+const {
+  createSession,
+  updateSession,
+} = require('../../controllers/sessionContorller');
+
+router.route('/user/session').post(createSession);
+
+router.route('/user/session').put(updateSession);
+router.route('/user').post(createUser);
+router.route('/user').delete(deleteUser);
+
+router.route('/user').put(updateBankroll);
