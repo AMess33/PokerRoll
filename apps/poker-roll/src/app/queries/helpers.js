90d005241a0bf535a1abc8ceda27dc0cffcb67eb
AMess33 <amess3309@gmail.com>
2023-10-02 20:11:18 -0500

    create a queries directory with helpers.js file, add mutations and queries in the helpers file, delete query and mutation usage in the homepage

---
+++ apps/poker-roll/src/app/queries/helpers.js
@@ -0,0 +1,49 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import {
+  getUser,
+  getSessions,
+  createSession,
+  updateSession,
+  deleteUser,
+} from '../my-api';
+
+export function useUser() {
+  return useQuery({ queryKey: ['user'], queryFn: getUser });
+}
+
+export function useSessions() {
+  return useQuery({ queryKey: ['sessions'], queryFn: getSessions });
+}
+
+export function useCreateSession() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: createSession,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['sessions'] });
+    },
+  });
+}
+
+export function useUpdateSession() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: updateSession,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['sessions'] });
+    },
+  });
+}
+
+export function useDeleteUser() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: deleteUser,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['user'] });
+    },
+  });
+}
