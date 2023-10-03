9dfb62bab0ae91662bbf0f0e1ea47196a756a44f
AMess33 <amess3309@gmail.com>
2023-10-02 21:26:18 -0500

    add use user and user query to bankroll header

--- apps/poker-roll/src/app/bankrollHeader.jsx
+++ apps/poker-roll/src/app/bankrollHeader.jsx
@@ -3,15 +3,17 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { useUser } from "./queries/helpers";
 
 const BankrollHeader = (props) => {
+    const userQuery = useUser()
 
     return (
         <div>
             <Card sx={{ maxWidth: 345 }}>
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
-                            Current BankRoll: {props.user.bankroll}
+                            Current BankRoll: {userQuery.data?.user.bankroll}
                     </Typography>
                     <Button variant="text">Add</Button>
                     <Button variant="text">Subtract</Button>
