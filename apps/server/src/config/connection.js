1db638fa56a10c339efaa161017ba32c034b0ebc
Andrew M <amess3309@gmail.com>
2023-10-05 20:00:50 -0700

    move connection string to .env file

--- apps/server/src/config/connection.js
+++ apps/server/src/config/connection.js
@@ -1,7 +1,7 @@
 const { connect, connection } = require('mongoose');
 
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb+srv://AMesser:bGHHP9MNANRkKS3a@cluster0.hrluy6c.mongodb.net/';
+  process.env.MONGODB_URI || ;
 
 connect(connectionString);
 
