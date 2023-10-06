580c8bc1729a8a27855144f79bb8e8a66bcffc46
Andrew M <amess3309@gmail.com>
2023-10-05 17:07:11 -0700

    add signup form jsx and basic form state functionality

--- apps/poker-roll/src/app/createUser.jsx
+++ apps/poker-roll/src/app/createUser.jsx
@@ -3,3 +3,85 @@ import React, { useState } from "react";
 import { TextField, Button, Stack } from "@mui/material";
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+
+function SignUpForm(){
+    const [formState, setFormState] = useState({
+        userName: "",
+        email: "",
+        password: "",
+    });
+
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+    
+        setFormState({
+          ...formState,
+          [name]: value,
+        });
+      };
+
+      const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        // create user functionality on form submit
+      };
+    return (
+        <div className="newAccountFormContainer">
+          <form
+            className="bg-white m-4 border border-dark rounded text-dark newAccountForm"
+            onSubmit={handleFormSubmit}
+          >
+            <Stack
+              spacing={2}
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <h2>Create an Account</h2>
+              <TextField
+                label="Username"
+                variant="outlined"
+                name="username"
+                type="text"
+                value={formState.name}
+                onChange={handleChange}
+                size="small"
+                margin="normal"
+              />
+              <TextField
+                name="email"
+                type="email"
+                value={formState.email}
+                label="Your Email"
+                onChange={handleChange}
+                variant="outlined"
+                size="small"
+                margin="normal"
+              />
+              <TextField
+                label="Password"
+                variant="outlined"
+                name="password"
+                type="password"
+                value={formState.password}
+                onChange={handleChange}
+                size="small"
+                margin="normal"
+              />
+    
+              <Button
+                type="submit"
+                size="small"
+                variant="contained"
+                color="primary"
+              >
+                Submit
+              </Button>
+            </Stack>
+          </form>
+        </div>
+      );
+    };
+    
+    export default Signup;
