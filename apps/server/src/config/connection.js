fdb1417ae6df1fab945b49935c4a653c5cd08147
Andrew M <amess3309@gmail.com>
2023-10-07 17:26:17 -0700

    add .env to the git ignore file, remove mongodb connection that wasnt used

--- apps/server/src/config/connection.js
+++ apps/server/src/config/connection.js
@@ -2,7 +2,7 @@ const { connect, connection } = require('mongoose');
 require('dotenv').config()
 
 const connectionString =
-  process.env.MONGODB_URI || process.env.DATABASE_CONNECTION;
+  process.env.DATABASE_CONNECTION;
 
 connect(connectionString);
 
