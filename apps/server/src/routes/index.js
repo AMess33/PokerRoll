d0af77184d2e2222ac65e5c6d2d14765300000af
Andrew M <amess3309@gmail.com>
2023-10-11 17:09:01 -0700

    remove create update and delete user functions from index.js in routes folder

--- apps/server/src/routes/index.js
+++ apps/server/src/routes/index.js
@@ -7,11 +7,7 @@
 
 // move this code to server/src/main.js
 const router = require('express').Router();
-const {
-  createUser,
-  deleteUser,
-  updateUser,
-} = require('../controllers/userController');
+
 const {
   createSession,
   updateSession,
@@ -24,9 +20,6 @@ router.route('/').get((req, res) => {
 router.route('/user/session').post(createSession);
 
 router.route('/user/session').put(updateSession);
-router.route('/user').post(createUser);
-router.route('/user/').delete(deleteUser);
 
-router.route('/user/').put(updateUser);
 
 module.exports = router;
