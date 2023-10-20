d5e18f8aa60f20135744c7a1ac6483efd75372d2
Andrew M <amess3309@gmail.com>
2023-10-20 13:55:14 -0500

    start building return stack for update session form

--- apps/poker-roll/src/app/updateSessionComponent.jsx
+++ apps/poker-roll/src/app/updateSessionComponent.jsx
@@ -10,5 +10,33 @@ function UpdateSessionForm(){
         location: "",
         outFor: "",
         notes: "",
-    })
+    });
+    const mutation = useGetAllSessions();
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    }
+    return(
+        <Stack 
+        spacing={2}
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        >
+        <TextField
+        label="Total In For"
+        variant="outlined"
+        name="buyIn"
+        type="text"
+        value={formState.buyIn}
+        onChange={handleChange}
+        size="small"
+        margin="normal"
+        />
+        </Stack>
+    )
 }
