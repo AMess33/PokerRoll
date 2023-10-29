374e89a5c8d24b685c01c4a1ee8ba36c6ae58b36
Andrew M <amess3309@gmail.com>
2023-10-28 22:53:30 -0500

    add hours and minutes to the date stamp function

--- apps/poker-roll/src/app/queries/helpers.js
+++ apps/poker-roll/src/app/queries/helpers.js
@@ -7,8 +7,10 @@ export function currentDateStamp() {
   const year = currentDate.getFullYear();
   const month = (currentDate.getMonth() + 1).toString().padStart(2, '0'); 
   const day = currentDate.getDate().toString().padStart(2, '0');
-  
-  return `${year}-${month}-${day}`;
+  const hours = currentDate.getHours().toString().padStart(2, '0');
+  const minutes = currentDate.getMinutes().toString().padStart(2, '0');
+
+  return `${year}-${month}-${day} ${hours}:${minutes}`;
 }
 
 export function timeStamp() {
