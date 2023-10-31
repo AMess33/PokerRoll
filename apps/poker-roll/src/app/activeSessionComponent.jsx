22f56cf2cffd0c76154982ec3e034f0d745c9b1a
Andrew M <amess3309@gmail.com>
2023-10-30 21:07:47 -0500

    create active session container to render active sessions one at a time and allow form flow for indivicual update session calls

--- apps/poker-roll/src/app/activeSessionComponent.jsx
+++ apps/poker-roll/src/app/activeSessionComponent.jsx
@@ -5,18 +5,22 @@ import UpdateSessionForm from "./updateSessionComponent";
 
 // get all sessions without an 'endTime' and display them as Active Sessions on the homepage
 
-const ActiveSession = () => {
-    const sessionsQuery = useGetAllSessions()
+const ActiveSession = (props) => {
+    
 
-    return sessionsQuery.data?.map(session => {
-        return (
-            <Container style={{border: '2px solid rgba(0, 0, 0, 0.05)'}} maxWidth="sm">
+    const [isUpdating, setIsUpdating] = React.useState(false);
+
+    const finishSession = (event, isFinished) => {
+        setIsUpdating(true);
+    }
+    return (
+    <Container style={{border: '2px solid rgba(0, 0, 0, 0.05)'}} maxWidth="sm">
                 <Box>
-                    <h4>{session.game}</h4>
-                    <p>{session.casino}, {session.location}</p>
+                    <h4>{props.session.game}</h4>
+                    <p>{props.session.casino}, {props.session.location}</p>
                     <Button
                     type="button"
-                    onClick={<UpdateSessionForm />}
+                    onClick={finishSession}
                     size="small"
                     variant="contained"
                     color="primary"
@@ -24,9 +28,21 @@ const ActiveSession = () => {
                         Finish Session
                     </Button>
                 </Box>
+                { isUpdating && <UpdateSessionForm/>}
             </Container>
-        )
-    })
-}
+    )
+};
+const ActiveSessionContainer = () => {
+    const sessionsQuery = useGetAllSessions()
+    return sessionsQuery.data?.map(session => { 
 
-export default ActiveSession;
+
+        return (
+            <ActiveSession
+            session={session}
+            />
+
+    )
+})};
+
+export default ActiveSessionContainer;
