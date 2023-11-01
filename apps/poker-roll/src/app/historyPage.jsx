5fffe4b54f907962a9ed264c0eee514b036cc6a4
Andrew M <amess3309@gmail.com>
2023-11-01 16:36:03 -0500

    add dayjs functions and formatting to history page and get rendering of dates to show on page correctly

--- apps/poker-roll/src/app/historyPage.jsx
+++ apps/poker-roll/src/app/historyPage.jsx
@@ -1,32 +1,42 @@
-import React from "react";
+import React from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import { useGetAllSessions } from "./queries/helpers";
+import { useGetAllSessions } from './queries/helpers';
+
+const dayjs = require('dayjs');
 
 // history page will show all past sessions
 
 const PastSession = () => {
-    const sessionsQuery = useGetAllSessions()
+  const sessionsQuery = useGetAllSessions();
 
-    return sessionsQuery.data?.map(session => {
-            return (
-                <Container maxWidth="sm">
-                    <Box>
-                        <h4>{session.startTime}</h4>
-                        <p>{session.game}</p>
-                        <p>{session.casino}, {session.location}</p>
-                        <span>{session.plusMinus}</span>
-                    </Box>
-                </Container>
-    )
-})};
-
-const History = (props) => {
+  return sessionsQuery.data?.map((session) => {
+    const start = dayjs(session.startTime);
+    const end = dayjs(session.endTime);
+    const duration = end.diff(`${start}`, 'hour', true).toFixed('1');
+    const formatDate = dayjs(session.startTime).format('MM/DD/YY');
 
     return (
-        <div>
-            <PastSession />
-        </div>
-    )
-}
+      <Container maxWidth="sm">
+        <Box>
+          <h4>{formatDate}</h4>
+          <p>{session.game}</p>
+          <p>
+            {session.casino}, {session.location}
+          </p>
+          <span>{duration} Hours</span>
+          <span>{session.plusMinus}</span>
+        </Box>
+      </Container>
+    );
+  });
+};
+
+const History = (props) => {
+  return (
+    <div>
+      <PastSession />
+    </div>
+  );
+};
 export default History;
