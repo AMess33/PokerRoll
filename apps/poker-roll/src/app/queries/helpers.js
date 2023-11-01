86f39537e38c553384dc0fd7b8e915efa36ef4fe
Andrew M <amess3309@gmail.com>
2023-10-31 23:21:09 -0500

    require dayjs in helper functions

--- apps/poker-roll/src/app/queries/helpers.js
+++ apps/poker-roll/src/app/queries/helpers.js
@@ -1,11 +1,14 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import { useUser } from "@clerk/clerk-react";
+import { useUser } from '@clerk/clerk-react';
+
+// use dayjs library for currentDateStamp and timeStamp functions
+const dayjs = require('dayjs');
 
 // set up a date.now function to be used in start time and end time and displayed as intended on app
 export function currentDateStamp() {
   const currentDate = new Date();
   const year = currentDate.getFullYear();
-  const month = (currentDate.getMonth() + 1).toString().padStart(2, '0'); 
+  const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
   const day = currentDate.getDate().toString().padStart(2, '0');
   const hours = currentDate.getHours().toString().padStart(2, '0');
   const minutes = currentDate.getMinutes().toString().padStart(2, '0');
@@ -26,32 +29,52 @@ export function timeStamp() {
 
 export function useSessions() {
   const user = useUser();
-  return useQuery({ queryKey: ['sessions', user.user.id], queryFn: () => {
-    return fetch(`http://localhost:3333/api/session?id=${user.user.id}`).then( (res) => res.json(),)
-  } });
+  return useQuery({
+    queryKey: ['sessions', user.user.id],
+    queryFn: () => {
+      return fetch(`http://localhost:3333/api/session?id=${user.user.id}`).then(
+        (res) => res.json()
+      );
+    },
+  });
 }
 
 export function useGetAllSessions() {
   const user = useUser();
-  return useQuery({ queryKey: ['sessions', 'all', user.user.id], queryFn: () => {
-    return fetch(`http://localhost:3333/api/allsessions?id=${user.user.id}`).then( (res) => res.json(),)
-  }})
+  return useQuery({
+    queryKey: ['sessions', 'all', user.user.id],
+    queryFn: () => {
+      return fetch(
+        `http://localhost:3333/api/allsessions?id=${user.user.id}`
+      ).then((res) => res.json());
+    },
+  });
 }
 
 export function useBankroll() {
   const user = useUser();
 
-  return useQuery({ queryKey: ['bankroll', user.user.id], queryFn: () => {
-    return fetch(`http://localhost:3333/api/bankroll?id=${user.user.id}`).then( (res) => res.json(),)
-  }});
+  return useQuery({
+    queryKey: ['bankroll', user.user.id],
+    queryFn: () => {
+      return fetch(
+        `http://localhost:3333/api/bankroll?id=${user.user.id}`
+      ).then((res) => res.json());
+    },
+  });
 }
 
 export function useGetAllBankroll() {
   const user = useUser();
 
-  return useQuery({ queryKey: ['bankroll', 'all', user.user.id], queryFn: () => {
-    return fetch(`http://localhost:3333/api/allbankroll?id=${user.user.id}`).then( (res) => res.json(),)
-  }})
+  return useQuery({
+    queryKey: ['bankroll', 'all', user.user.id],
+    queryFn: () => {
+      return fetch(
+        `http://localhost:3333/api/allbankroll?id=${user.user.id}`
+      ).then((res) => res.json());
+    },
+  });
 }
 
 export function useCreateSession() {
@@ -61,8 +84,12 @@ export function useCreateSession() {
   return useMutation({
     mutationFn: (formState) => {
       console.log(formState);
-        return fetch('http://localhost:3333/api/session', {method: "POST", body: JSON.stringify({...formState, userID: user.user.id}), headers: { "Content-Type": "application/json",}})
-      },
+      return fetch('http://localhost:3333/api/session', {
+        method: 'POST',
+        body: JSON.stringify({ ...formState, userID: user.user.id }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['sessions'] });
     },
@@ -75,9 +102,13 @@ export function useUpdateSession() {
   return useMutation({
     mutationFn: (formState) => {
       console.log(formState);
-      return fetch('http://localhost:3333/api/session', {method: 'PUT', body: JSON.stringify(formState), headers: {
-        "Content-Type": "application/json",
-      }})
+      return fetch('http://localhost:3333/api/session', {
+        method: 'PUT',
+        body: JSON.stringify(formState),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['sessions'] });
@@ -85,16 +116,20 @@ export function useUpdateSession() {
   });
 }
 
-
 export function useGetBankroll() {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: (formState) => {
       console.log(formState);
-        return fetch('http://localhost:3333/api/bankroll', {method: 'POST', body: JSON.stringify(formState),  headers: {
-          "Content-Type": "application/json",} })
-      },
+      return fetch('http://localhost:3333/api/bankroll', {
+        method: 'POST',
+        body: JSON.stringify(formState),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['bankroll'] });
     },
@@ -108,11 +143,14 @@ export function useUpdateBankroll() {
   return useMutation({
     mutationFn: (formState) => {
       console.log(formState);
-      return fetch('http://localhost:3333/api/bankroll', {method: "POST", body: JSON.stringify({...formState, userID: user.user.id}), headers: { "Content-Type": "application/json",}})
+      return fetch('http://localhost:3333/api/bankroll', {
+        method: 'POST',
+        body: JSON.stringify({ ...formState, userID: user.user.id }),
+        headers: { 'Content-Type': 'application/json' },
+      });
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['bankroll'] });
     },
   });
 }
-
