ad74f654988438a8ca6349e22f14b230ba56b39b
Andrew M <amess3309@gmail.com>
2023-11-02 22:07:02 -0500

    add filter function to remove any sessions that do not have an endtime yet

--- apps/poker-roll/src/app/historyPage.jsx
+++ apps/poker-roll/src/app/historyPage.jsx
@@ -10,24 +10,23 @@ const dayjs = require('dayjs');
 const PastSession = () => {
   const sessionsQuery = useGetAllSessions();
 
-  return sessionsQuery.data?.map((session) => {
-    if (!session.endTime) {
-      return null;
-    }
-    const start = dayjs(session.startTime);
-    const end = dayjs(session.endTime);
-    const duration = end.diff(start, 'hour', true).toFixed('1');
-    const formatDate = dayjs(session.startTime).format('MM/DD/YY');
-    const inFor = Number(`${session.buyIn}`);
-    const outFor = Number(`${session.outFor}`);
-    const plusMinus = Math.floor(outFor - inFor);
-    const perHour = Math.floor(plusMinus / duration);
+  return sessionsQuery.data
+    ?.filter((session) => session.endTime)
+    .map((session) => {
+      const start = dayjs(session.startTime);
+      const end = dayjs(session.endTime);
+      const duration = end.diff(start, 'hour', true).toFixed('1');
+      const formatDate = dayjs(session.startTime).format('MM/DD/YY');
+      const inFor = Number(`${session.buyIn}`);
+      const outFor = Number(`${session.outFor}`);
+      const plusMinus = Math.floor(outFor - inFor);
+      const perHour = Math.floor(plusMinus / duration);
 
-    return (
-      <div key={session.id}>
+      return (
         <Container
           style={{ border: '2px solid rgba(0, 0, 0, 0.5)' }}
           maxWidth="sm"
+          key={session.id}
         >
           <Box>
             <h4>{formatDate}</h4>
@@ -40,9 +39,8 @@ const PastSession = () => {
             <span> ${perHour} /hour</span>
           </Box>
         </Container>
-      </div>
-    );
-  });
+      );
+    });
 };
 
 const History = (props) => {
