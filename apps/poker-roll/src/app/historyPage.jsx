094513298345934403b1386dca898e16de129d3f
Andrew M <amess3309@gmail.com>
2023-11-11 22:48:29 -0600

    accordion component is working, did some styling and spacing of text

--- apps/poker-roll/src/app/historyPage.jsx
+++ apps/poker-roll/src/app/historyPage.jsx
@@ -15,49 +15,60 @@ const PastSession = (props) => {
 
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
-
-    const start = dayjs(props.session.startTime);
-    const end = dayjs(props.session.endTime);
-    const duration = end.diff(start, 'hour', true).toFixed('1');
-    const formatDate = dayjs(props.session.startTime).format('MM/DD/YY');
-    const inFor = Number(`${props.session.buyIn}`);
-    const outFor = Number(`${props.session.outFor}`);
-    const plusMinus = Math.floor(outFor - inFor);
-    const perHour = Math.floor(plusMinus / duration);
-
-    return (
-      <div>
-        <h2>Completed Sessions</h2>
-        <Accordion
-          expanded={expanded === props.session._id}
-          onChange={handleChange(props.session._id)}
-        >
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1bh-content"
-            id="panel1bh-header"
-          >
-            <Typography sx={{ width: '33%', flexShrink: 0 }}>
-              {formatDate}
-            </Typography>
-            <Typography sx={{ color: 'text.secondary' }}>
-              {props.session.game}
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              <div>
-                {props.session.casino}, {props.session.location}
-              </div>
-              <span> {duration} Hours </span>
-              <span> ${plusMinus} </span>
-              <span> ${perHour} /hour</span>
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-      </div>
-    );
   };
+
+  const start = dayjs(props.session.startTime);
+  const end = dayjs(props.session.endTime);
+  const duration = end.diff(start, 'hour', true).toFixed('1');
+  const formatDate = dayjs(props.session.startTime).format('MM/DD/YY');
+  const inFor = Number(`${props.session.buyIn}`);
+  const outFor = Number(`${props.session.outFor}`);
+  const plusMinus = Math.floor(outFor - inFor);
+  const perHour = Math.floor(plusMinus / duration);
+
+  return (
+    <div className="accordions">
+      <Accordion
+        expanded={expanded === props.session._id}
+        onChange={handleChange(props.session._id)}
+        sx={{
+          backgroundColor: 'gold',
+        }}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+        >
+          <Typography
+            sx={{
+              width: '33%',
+              flexShrink: 0,
+              color: 'blue',
+              fontWeight: 'bold',
+              borderColor: 'gold',
+            }}
+          >
+            {props.session.casino}
+          </Typography>
+          <Typography sx={{ width: '75%', color: 'text.secondary' }}>
+            {props.session.game}
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            <span>{props.session.location}</span>
+            <br />
+            {duration} Hours &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Profit: ${plusMinus}{' '}
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ${perHour} /hour
+            <br />
+            <span>{props.session.notes}</span>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
 };
 const PastSessionContainer = () => {
   const sessionsQuery = useGetAllSessions();
