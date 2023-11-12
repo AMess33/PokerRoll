ce02d0222d8f625459972904e137a255f7b05d7b
Andrew M <amess3309@gmail.com>
2023-11-11 21:20:45 -0600

    working on building the accordion componenet in the past session function

--- apps/poker-roll/src/app/historyPage.jsx
+++ apps/poker-roll/src/app/historyPage.jsx
@@ -1,56 +1,69 @@
 import React from 'react';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useGetAllSessions } from './queries/helpers';
 
 const dayjs = require('dayjs');
 
 // history page will show all past sessions
 
-const PastSession = () => {
-  const sessionsQuery = useGetAllSessions();
+const PastSession = (props) => {
+  const [expanded, setExpanded] = React.useState(false);
 
-  return sessionsQuery.data
-    ?.filter((session) => session.endTime)
-    .map((session) => {
-      const start = dayjs(session.startTime);
-      const end = dayjs(session.endTime);
-      const duration = end.diff(start, 'hour', true).toFixed('1');
-      const formatDate = dayjs(session.startTime).format('MM/DD/YY');
-      const inFor = Number(`${session.buyIn}`);
-      const outFor = Number(`${session.outFor}`);
-      const plusMinus = Math.floor(outFor - inFor);
-      const perHour = Math.floor(plusMinus / duration);
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
 
-      return (
-        <Container
-          sx={{ mt: 2 }}
-          style={{ border: '2px solid rgba(0, 0, 0, 0.5)' }}
-          maxWidth="sm"
-          key={session._id}
+    const start = dayjs(props.session.startTime);
+    const end = dayjs(props.session.endTime);
+    const duration = end.diff(start, 'hour', true).toFixed('1');
+    const formatDate = dayjs(props.session.startTime).format('MM/DD/YY');
+    const inFor = Number(`${props.session.buyIn}`);
+    const outFor = Number(`${props.session.outFor}`);
+    const plusMinus = Math.floor(outFor - inFor);
+    const perHour = Math.floor(plusMinus / duration);
+
+    return (
+      <div>
+        <h2>Completed Sessions</h2>
+        <Accordion
+          expanded={expanded === props.session._id}
+          onChange={handleChange(props.session._id)}
         >
-          <Box>
-            <h4 style={{ margin: '0', textAlign: 'center' }}>
-              {formatDate} {session.game}
-            </h4>
-            <div>
-              {session.casino}, {session.location}
-            </div>
-            <span> {duration} Hours </span>
-            <span> ${plusMinus} </span>
-            <span> ${perHour} /hour</span>
-          </Box>
-        </Container>
-      );
-    });
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1bh-content"
+            id="panel1bh-header"
+          >
+            <Typography sx={{ width: '33%', flexShrink: 0 }}>
+              {formatDate}
+            </Typography>
+            <Typography sx={{ color: 'text.secondary' }}>
+              {props.session.game}
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              <div>
+                {props.session.casino}, {props.session.location}
+              </div>
+              <span> {duration} Hours </span>
+              <span> ${plusMinus} </span>
+              <span> ${perHour} /hour</span>
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      </div>
+    );
+  };
+};
+const PastSessionContainer = () => {
+  const sessionsQuery = useGetAllSessions();
+  return sessionsQuery.data?.map((session) => {
+    return <PastSession key={session._id} session={session} />;
+  });
 };
 
-const History = (props) => {
-  return (
-    <div>
-      <h2>Completed Sessions</h2>
-      <PastSession />
-    </div>
-  );
-};
-export default History;
+export default PastSessionContainer;
