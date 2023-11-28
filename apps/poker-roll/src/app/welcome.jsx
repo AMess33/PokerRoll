6c0778694bab08558227435697e32a34f87aa22d
Andrew M <amess3309@gmail.com>
2023-11-27 19:54:41 -0600

    add styling to welcome component

--- apps/poker-roll/src/app/welcome.jsx
+++ apps/poker-roll/src/app/welcome.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Welcome = () => {
   return (
-    <div>
+    <div className="welcome">
       <h1>Welcome to Poker Roll</h1>
+      <div>
+        <p>Open the BankRoll tab to adjust your Bankroll</p>
+        <p>Start a new session to track your win loss over time</p>
+      </div>
     </div>
   );
 };
